8c94747c6d223f0aca4aa6bed1b85fbbedb383fd
Hormiks <romerodaniela957@gmail.com>
2025-05-05 16:30:21 -0400

    Corregido :D

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-const studets=[];
-const tablebody=document
+// Se necesita capturar, pasar el dato del formulario y guardarlo en un array
+const students=[];
 
-document.getElementById("studentForm").addEventListener("sumbit",function(e){
+const tableBody=document.querySelector("#studentsTable tbody");
+// Se coloca ID del formulario
+// addEventListener escucha cuando alguien hace click en el boton submit
+document.getElementById("studentForm").addEventListener("submit",function(e){
     e.preventDefault();
 
+    //trim elimina los espacios
     const name=document.getElementById("name").value.trim();
     const lastName=document.getElementById("lastName").value.trim();
     const grade=document.getElementById("grade").value.trim();
+    const date=document.getElementById("date").value.trim();
 
-    if (grade <1 || grade >7 || !name || !lastName || isNaN(grade)){
+    //Se debe validar cuando haya algún dato que no corresponda. Como una nota menor que 1 y mayor que 7. Entonces se debe programar una alerta de error.
+    if(grade <1 || grade >7 || !name || !lastName || isNaN(grade)){
         alert("Error Datos Incorrectos")
         return
     }
-    //guardar datos en el Array
 
-    const studets=(name,lastName,grade);
-    studets.push(studets);
-    addStudentToTable(studets); 
+    //Guardar datos en el Array
 
-
-    //console.log(studets)
+    const student={name,lastName,grade,date};
+    students.push(student);
+    addStudentToTable(student)
+    //console.log(students)
     this.reset();
+
 });
-
-function addStudentToTable(studets){
-    const row=document.createElement("tr")
+// Se deben guardar los estudiantes en la tabla. Se le pasan los student a la función
+function addStudentToTable(student){
+    const row=document.createElement("tr");
     row.innerHTML=
-    `<td>${studets.name}</td>
-    <td>${studets.lastName}</td>
-    <td>${studets.grade}</td>`;
-    tablebody.appendChild(row);
+    `<td>${student.name}</td>
+    <td>${student.lastName}</td>
+    <td>${student.grade}</td>
+    <td>${student.date}</td>`;
+    tableBody.appendChild(row);
 }
-
